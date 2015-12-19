9f4f89a5bc09ed8bd65af83be976de1323d30f43
Chris Joiner <cjoiner20@hotmail.co.uk>
2015-12-19 12:35:11 +0000

    added new controller and view files for Orders

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'static_pages#index'
+  root 'static_pages#landing_page'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -22,6 +22,8 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+
+  resources :orders, only: [:index, :show, :create, :destroy]
 
   # Example resource route with options:
   #   resources :products do
