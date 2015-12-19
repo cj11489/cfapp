eda02e1a61948596080afe3331c10501a4f98cd2
Chris Joiner <cjoiner20@hotmail.co.uk>
2015-12-19 17:29:44 +0000

    deleted and recreated orders db due to spelling mistake, created order model and associated to products

--- db/migrate/20151219165912_create_orders.rb
+++ db/migrate/20151219165912_create_orders.rb
@@ -1,8 +1,8 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
-      t.interger :user_id
-      t.interger :product_id
+      t.integer :user_id
+      t.integer :product_id
       t.float :total
     end
     add_index :orders, :user_id
