167232cec7e39ab97210a242ffca0efd90be0a71
Chris Joiner <cjoiner20@hotmail.co.uk>
2016-01-23 18:13:41 +0000

    created scaffolding for users section, added form_tag to application.html.erb file to add in search bar function. Styled the search bar using bootstrap and added classes to the form fields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151219165912) do
+ActiveRecord::Schema.define(version: 20160123172003) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 20151219165912) do
     t.datetime "updated_at",  null: false
     t.string   "colour"
     t.string   "price"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email_address"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
 end
