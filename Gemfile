95d3f9a51b177057eb1ba56d4ed5160df4f21779
Chris Joiner <cjoiner20@hotmail.co.uk>
2016-02-18 14:29:03 +0000

    added Rspec gem and installed. Created spec for products model testing average ratings of products

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 #Ruby
-ruby "2.2.4"
+ruby "2.2.1"
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.4'
@@ -9,6 +9,7 @@ gem 'rails', '4.2.4'
 # Use sqlite3 as the database for Active Record
 group :development, :test do  
 	gem 'sqlite3'
+  	gem 'rspec-rails', '~> 3.0'
 end
 
 # Use SCSS for stylesheets
---
+++ spec/models/product_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe Product do
+	context "when a product has comments" do
+		before do
+			@product = Product.create!(name: "birthday card")
+			@user = User.create!(first_name: "Chris", last_name: "Joiner", email: "cjoiner20@hotmail.co.uk", password:"password")
+			@product.comments.create!(rating: 1, user: @user, body: "terrible product")
+			@product.comments.create!(rating: 3, user: @user, body: "not bad")
+			@product.comments.create!(rating: 5, user: @user, body: "Amazing!")
+		end
+
+		it "returns an average rating of all comments" do
+			expect(@product.average_rating).to eq 3
+		end
+
+		it "is not valid" do
+			expect(Product.new(description: "5 letters to a line")).not_to be_valid
+			expect(Product.new(colour: "red")).not_to be_valid
+		end
+
+	end
+end
