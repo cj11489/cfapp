35dc1f8c635dd64df9463e5cb156a042e750faba
Chris Joiner <cjoiner20@hotmail.co.uk>
2016-02-20 12:16:33 +0000

    moved comment method from comment model to comment controller so alert messages displayed

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,19 +5,4 @@ class Comment < ActiveRecord::Base
   	validates :user, presence: true
   	validates :product, presence: true
   	validates :rating, numericality: { only_integer: true}
-
-  def create
-    @product = Product.find(params[:product_id])
-    @comment = @product.comments.new(comment_params)
-    @comment.user = current_user
-    respond_to do |format|
-      if @comment.save
-        format.html { redirect_to @product, notice: 'Thank you, your review has been received' }
-        format.json { render :show, status: :created, location: @product }
-      else
-        format.html { redirect_to @product, alert: 'Sorry, but your review could not be saved' }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
 end
