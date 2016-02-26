b9989dd325e73170bf9269afa18adaa9b454d9d4
Chris Joiner <cjoiner20@hotmail.co.uk>
2016-02-26 13:41:33 +0000

    added elevateZoom.js to javascript folder and implemented so that users can hover over the product images to zoom in. Also added jquery-ui.js which provides a number of widgets. Of these I have implemented an autocomplete function so it provides suggestions as users type into the search bar

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -6,4 +6,14 @@ $(document).on('ready page:load', function() {
         return $(this).attr('data-score');
       }
     });
+
+    //elevate zoom jquery plugin - allows users to zoom in on product images
+    $('.img-zoom').elevateZoom();
+
+    //Autocomplete jquery plugin - defining autocomplete source list
+    $(".selector").autocomplete({
+    	source: [ "1-3 Letters", "4-6 Letters", "7-10 Letters", "banana"]
+    })
+
+    $(".selector").autocomplete("option", "source", ["1-3 Letters", "4-6 Letters", "7-10 Letters", "banana"])
 })
