dd1ba62241d38193a66bf7bf48901b7a1f0b9bdd
Chris Joiner <cjoiner20@hotmail.co.uk>
2016-03-06 19:46:57 +0000

    added if user_signed_in? to product show page to ensure users are signed in before trying to purchase product

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,5 +1,6 @@
 class PaymentsController < ApplicationController
-
+	before_filter :authenticate_user!
+	
 	def show
 	end
 
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
   respond_to :json, :html
+  
 
   # GET /products
   # GET /products.json
